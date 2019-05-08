53ea43475f6aebf74ff552956b19d98d0dbea8c6
Anoop Raveendran <anooprav7@gmail.com>
2019-05-08 16:18:15 +0530

    renamed to app

--- packages/react-scripts/template/src/routing/index.js
+++ packages/react-scripts/template/src/routing/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from 'pages/Home';
+import Home from 'app/Home';
 
 class Routing extends React.Component {
   render() {
